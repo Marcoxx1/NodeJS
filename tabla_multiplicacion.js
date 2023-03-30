c20afbe7fca8f2b97939a445d6e53198c80a23e0
Your Name <you@example.com>
2023-03-30 16:10:19 -0700

    No se que se hizo, pero algo se hizo

--- tabla_multiplicacion.js
+++ tabla_multiplicacion.js
@@ -1,7 +1,11 @@
+const argv = require("yargs").argv;
 var colors = require("colors");
 
-/*const {crearArchivo} = require("./helpers/multiplicar")
-
+const {crearArchivo} = require("./helpers/multiplicar")
+/*
 crearArchivo(3);*/
 
-console.log("OMG Rainbows!".rainbow);
+//*console.log("OMG Rainbows!".rainbow);
+console.log(argv);
+console.log(argv.base);
+//*crearArchivo(argv.base);
